177985ab6d71b11a58799c10f722e712597a3a8b
Henry Recker <henryjrecker@gmail.com>
2020-03-22 17:26:44 -0500

    Created initial map scene with generated tilemap

---
+++ Assets/Scripts/Levels/CurrentLevel.cs
@@ -0,0 +1,36 @@
+﻿public class CurrentLevel
+{
+    private static TileType[,] tiles;
+
+    public static void generateLevel(int width, int height)
+    {
+        tiles = new TileType[width, height];
+        //TODO actual generation logic
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x == 0 || y == 0 || x == width - 1 || y == height - 1)
+                {
+                    tiles[x, y] = TileType.WALL;
+                }
+                else
+                {
+                    tiles[x, y] = TileType.FLOOR;
+                }
+            }
+        }
+    }
+
+    public static TileType[,] getTiles()
+    {
+        return tiles;
+    }
+}
+
+public enum TileType
+{
+    FLOOR,
+    WALL,
+    STAIRS
+}
---
+++ Assets/Scripts/Levels/TilemapPainter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapPainter : MonoBehaviour
+{
+    //TODO these probably could go in some config file somewhere
+    public int width, height;
+    public Tilemap tileMap;
+
+    void Awake()
+    {
+        //TODO won't want to do this on awake, only when new level is needed
+        CurrentLevel.generateLevel(width, height);
+        paintLevel(CurrentLevel.getTiles());
+    }
+
+    private void paintLevel(TileType[,] tiles)
+    {
+        tileMap.ClearAllTiles();
+        int xLength = tiles.GetLength(0);
+        int yLength = tiles.GetLength(1);
+        for (int x = 0; x < xLength; x++)
+        {
+            for (int y = 0; y < yLength; y++)
+            {
+                CustomTile tile = ScriptableObject.CreateInstance<CustomTile>();
+                tile.Init(tiles[x, y]);
+                tileMap.SetTile(new Vector3Int(x - (xLength / 2), y - (yLength / 2), 0), tile);
+            }
+        }
+    }
+}
